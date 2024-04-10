fc2715fc8a6afcb26f8fea17c3a835a1f37d67ae
Anish Ummenthala <ummenthala.anish@gmail.com>
2024-04-10 14:31:52 -0700

    shadcn initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button";
 import { FC } from "react";
 
 export interface HomePageProps {}
 
 const HomePage: FC<HomePageProps> = () => {
-  return <main>Anish Ummenthala Blog</main>;
+  return (
+    <main className="flex flex-col gap-4 items-center justify-center min-h-dvh">
+      Anish Ummenthala Blog
+      <Button>Click Me !</Button>
+    </main>
+  );
 };
 
 export default HomePage;
